2b7998826b2e44944e522bba31b0bbb9307cdfd7
Jacob Larsson <jacla460@student.liu.se>
2019-07-29 11:50:37 +0200

    Added existing directory files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float dragCoeff;
+    private int score;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.drag = dragCoeff;
+        score = 0;
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            scoreText.text = "Score: " + score;
+        }
+    }
+}
